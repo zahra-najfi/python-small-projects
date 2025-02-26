a243379e56361976e507ea7aae73af4679caa21c
Zahra Najfi <zahranajfics@gmail.com>
2025-02-25 22:17:51 -0800

    Add new programe

---
+++ sumof first N natural numbers.py
@@ -0,0 +1,4 @@
+a = int(input("Enter a number: "))  # User se number lena
+
+for i in range(1, a + 1):  # Loop 1 se a tak chalega
+    print(i, end="  ")  # Numbers ek line me space ke saath print honge
