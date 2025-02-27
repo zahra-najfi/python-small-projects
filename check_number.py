1ddf16efd2c4a1bacaa8909b15b39435a2237ef5
Zahra Najfi <zahranajfics@gmail.com>
2025-02-26 21:10:43 -0800

    Check Number

---
+++ check_number.py
@@ -0,0 +1,9 @@
+num=(float(input("Enter a number")))
+if num<0:
+    print("number is negative")
+elif num>0:
+    print("Number is positive")
+elif num==0:
+    print("number is zero")
+else:
+    print("Invalid input")            
